8dd5064cba000692754f1a93b05955b02d77d80f
Pai22 <paisrp22@gmail.com>
2024-08-19 11:30:14 +0700

    add state-func.js
    add state-func.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from'react';
 //import Button from './class-components';
 //import { Calculator2 } from './calculator';
 //import { EvenData2 } from './event-data';
-import RefsFunc from './refs-func';
+//import RefsFunc from './refs-func';
+import MessageBox from './state-func';
 
 function App() {
   return (
     //<Button />
     //<Calculator2 />
     //<EvenData2 />
-    <RefsFunc />
+    //<RefsFunc />
+    <MessageBox />
   )
 }
 
