fb9f00e9cbaf4fe3098840ec585a347ce6b457a6
Pai22 <paisrp22@gmail.com>
2024-08-19 10:16:01 +0700

    Addrefs-func.js
    Addrefs-func.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from'react';
 //import Button from './class-components';
 //import { Calculator2 } from './calculator';
-import { EvenData2 } from './event-data';
+//import { EvenData2 } from './event-data';
+import RefsFunc from './refs-func';
 
 function App() {
   return (
     //<Button />
     //<Calculator2 />
-    <EvenData2 />
+    //<EvenData2 />
+    <RefsFunc />
   )
 }
 
