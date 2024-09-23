878e68471352af2cf0f62ce16ae4b0391cafe8b3
Pai22 <paisrp22@gmail.com>
2024-09-23 11:21:23 +0700

    Add router3 and router4
    Add router3 and router4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from'react';
-import Router2 from './router2';
+import Router3 from './router3';
+// import Router4 from './router4';
 
 
 function App() {
   return (
-    <Router2 />
+    <Router3 />
+    // <Router4 />
 
   )
 }
