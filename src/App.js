9d692a70d6ce98e1b9031bc643f8a3c8211bbe07
Pai22 <paisrp22@gmail.com>
2024-09-30 11:32:21 +0700

    Do react with  bootstrap
    Do react with  bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from'react';
-import Router3 from './router3';
+
+import FormSelectFile from './form-select-file';
 // import Router4 from './router4';
 
 
 function App() {
   return (
-    <Router3 />
-    // <Router4 />
+   <FormSelectFile/>
 
   )
 }
